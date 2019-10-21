45c7b4e366916e005541df3053932161468091de
Mikulasek <mikulasek.viktor.cz@gmail.com>
2019-10-21 21:16:48 +0200

    add aps

--- spring-demo-aop/src/cz/vibri/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/cz/vibri/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,8 +11,32 @@ public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging
 
 	// let's start with an @Before advice
-	@Before("execution(public void addAccount())")
-	public void beforeAddAccountAdvice() {
-		System.out.println("\n ===> Executing @Before advice on addAccount()");
+//	@Before("execution(public void addAccount())")
+//	public void beforeAddAccountAdvice() {
+//		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+//	}
+	
+	// fully qualified detection
+//	@Before("execution(public void cz.vibri.aopdemo.dao.AccountDao.addAccount())")
+//	public void beforeFullyQualifiedAddAccountAdvice() {
+//		System.out.println("\n=====>>> Executing @Before fully qualified advice on addAccount()");
+//	}
+	
+	// any method with add prefix in method name
+//	@Before("execution(* add*(cz.vibri.aopdemo.Account, ..))")
+//	public void beforeAnyAddAdvice() {
+//		System.out.println("\n=====>>> Executing @Before advice on any add*()");
+//	}
+	
+	// any method with add prefix in method name and an number of parameters
+//	@Before("execution(* add*(..))")
+//	public void beforeAnyAddAdviceAnyParameters() {
+//		System.out.println("\n=====>>> Executing @Before advice on any add*()");
+//	}
+	
+	// pointcut on methods in package
+	@Before("execution(* cz.vibri.aopdemo.dao.*.*(..))")
+	public void beforeAnyMethodInPackage() {
+		System.out.println("\n=====>>> Executing @Before advice on any method in package");
 	}
 }
